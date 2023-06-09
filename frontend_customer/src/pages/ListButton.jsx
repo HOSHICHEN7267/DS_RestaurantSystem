0a0db5b7bf15ce5b2f2590e76bbaa9ec979e0bcb
HOSHICHEN7267 <68357482+HOSHICHEN7267@users.noreply.github.com>
2023-06-09 21:04:13 +0800

    post completed, need testing

--- frontend_customer/src/pages/ListButton.jsx
+++ frontend_customer/src/pages/ListButton.jsx
@@ -3,9 +3,48 @@ import styles from "./CustomerEnd.module.css";
 
 function ListButton() {
   const [state, setState] = useState("order");
+  const url = "http://127.0.0.1:5000/customer/orders"; // url to fetch
+  let orderID = ""; // the ID returned by etcd
+
+  // a fake order
+  const order = {
+    table_number: 5,
+    foods: [
+      {
+        name: "karaagedon",
+        price: 120,
+        quantity: 2
+      },
+      {
+        name: "porkdon",
+        price: 160,
+        quantity: 1
+      },
+      {
+        name: "katsudon",
+        price: 130,
+        quantity: 3
+      },
+    ]
+  };
 
   const orderButton = () => {
     // create an order
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(order)
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('POST 請求成功:', data); // should save id to orderID here
+      })
+      .catch(error => {
+        console.error('發生錯誤:', error);
+      });
+
     setState("cancel");
   };
 
@@ -23,23 +62,23 @@ function ListButton() {
     case "order":
       return (
         <button className={styles.orderButton} onClick={orderButton}>
-            <div className={styles.description}>確認送出</div>
+          <div className={styles.description}>確認送出</div>
         </button>
       );
     case "cancel":
       return (
         <button className={styles.cancelButton} onClick={cancelButton}>
-            <div className={styles.description}>取消訂單</div>
+          <div className={styles.description}>取消訂單</div>
         </button>
       );
     case "preparing":
       return (
         <button className={styles.preparingButton} onClick={preparingButton}>
-            <div className={styles.description}>製作中</div>
+          <div className={styles.description}>製作中</div>
         </button>
       );
   }
-  
+
 }
 
 export default ListButton;
