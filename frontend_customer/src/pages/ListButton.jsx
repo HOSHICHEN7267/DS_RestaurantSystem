b177ca2695b9ffaf259d781f35d8378817173471
HOSHICHEN7267 <68357482+HOSHICHEN7267@users.noreply.github.com>
2023-06-11 13:49:46 +0800

    second order will reset now

--- frontend_customer/src/pages/ListButton.jsx
+++ frontend_customer/src/pages/ListButton.jsx
@@ -27,6 +27,10 @@ function ListButton(props) {
       }
       else if(data.status == "done"){
         setState("order");
+        for(food in order.foods){
+          food.quantity = 0;
+          food.price = 0;
+        }
       }
     }
   });
