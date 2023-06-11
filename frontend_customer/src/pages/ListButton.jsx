0de0104e9663c62bdf7098f2fcb92157efff759e
HOSHICHEN7267 <68357482+HOSHICHEN7267@users.noreply.github.com>
2023-06-11 18:01:21 +0800

    website can show now

--- frontend_customer/src/pages/ListButton.jsx
+++ frontend_customer/src/pages/ListButton.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from "./CustomerEnd.module.css";
 // import io from 'socket.io-client';
 
